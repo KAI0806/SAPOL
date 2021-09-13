728a12569334b2b045f67703e576c7321afcaa51
MASATAKE_KAI <nerotan0806@gmail.com>
2021-09-13 15:14:25 +0900

    スライダー追加

---
+++ slider.js
@@ -0,0 +1,12 @@
+$('.slider').slick({ //{}を入れる
+    autoplay: true,
+    autoplaySpeed: 4000, //「オプション名: 値」の形式で書く
+	  infinite: true,　　　　//スライドのループ
+	  pauseOnHover: false,
+    prevArrow: '<button class="slide-arrow prev-arrow">＜</button>',
+    nextArrow: '<button class="slide-arrow next-arrow">＞</button>',
+    adaptiveHeight:true,
+    swipe: true,
+    verticalSwiping: false,
+    speed: 1000
+  });
